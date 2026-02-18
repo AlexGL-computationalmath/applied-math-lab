d8555d2d8ebd0c1628d4bb1eb87d1f0d18e53948
Alex GL <261989469+AlexGL-computationalmath@users.noreply.github.com>
2026-02-18 11:46:27 +0100

    Implement modular math utilities architecture (Day 2)

---
+++ projects/00-foundations/use_math_utils.py
@@ -0,0 +1,16 @@
+import math_utils
+
+
+def main():
+    print("Testing math utilities")
+
+    number = float(input("Enter a number: "))
+
+    print("Square:", math_utils.square(number))
+    print("Cube:", math_utils.cube(number))
+
+
+
+
+if __name__ == "__main__":
+    main()
